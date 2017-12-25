b7561b9852a46af51f3dbe8565a48f265dde20d4
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-25 11:57:55 +0000

    Add increase and decrease temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -10,19 +10,28 @@ describe("A thermostat", function(){
   });
 
   it("Should be in powersaving mode by default", function(){
-  expect(thermostat.isPowerSaving()).toBe(true);
+    expect(thermostat.isPowerSaving()).toBe(true);
   });
 
   it("Can turn powersaving off", function(){
-  thermostat.powerSavingOff();
-  expect(thermostat.isPowerSaving()).toBe(false);
-})
+    thermostat.powerSavingOff();
+    expect(thermostat.isPowerSaving()).toBe(false);
+  });
 
   it("Can turn powersaving on", function(){
-  thermostat.powerSavingOff();
-  thermostat.powerSavingOn();
-  expect(thermostat.isPowerSaving()).toBe(true);
-})
+    thermostat.powerSavingOff();
+    thermostat.powerSavingOn();
+    expect(thermostat.isPowerSaving()).toBe(true);
+  });
 
+  it("Can increase the temperature", function(){
+    thermostat.up()
+    expect(thermostat.getCurrentTemperature()).toEqual(21)
+  });
+
+  it("Can decrease the temperature", function(){
+    thermostat.down()
+    expect(thermostat.getCurrentTemperature()).toEqual(19)
+  });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,6 +3,9 @@
 function Thermostat (){
   this.temperature = 20;
   this.powerSaving = true;
+  this.MIN_TEMP = 10;
+  this.MAX_TEMP_POWERSAVING = 25;
+  this.MAX_TEMP = 32;
 };
 
 Thermostat.prototype.isPowerSaving = function(){
@@ -14,9 +17,21 @@ Thermostat.prototype.getCurrentTemperature = function(){
 }
 
 Thermostat.prototype.powerSavingOn = function(){
+  if(this.temperature > this.MAX_TEMP_POWERSAVING){ this.temperature = 25 }
   this.powerSaving = true;
 };
 
 Thermostat.prototype.powerSavingOff = function(){
   this.powerSaving = false;
 };
+
+Thermostat.prototype.up = function(){
+  if(this.powerSaving && this.temperature >= this.MAX_TEMP_POWERSAVING){throw Error("Max temperature is 25 when power saving is on")};
+  if(!this.powerSaving && this.temperature >= this.MAX_TEMP){throw Error("Max temperature is 32")}
+  this.temperature ++;
+}
+
+Thermostat.prototype.down = function(){
+  if(this.temperature === this.MIN_TEMP){ throw Error("The lowest temperature is 10") }
+  this.temperature --;
+}
